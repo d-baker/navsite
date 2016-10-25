6367c40d93b9961a2ad3a5e74133cafcf1ee2101
dbaker <d-_-baker@hotmail.com>
2016-10-25 17:11:51 +1100

    begin inclusion of audio players in av info boxes

--- assets/js/AVInfoToggle.js
+++ assets/js/AVInfoToggle.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-	$("p.descrip").hide();
+	$(".descrip").hide();
     $("a.info").click(function(e) {
         var $target = $(e.currentTarget);
-        var desc = $target.parents("h2").siblings("p.descrip");
+        var desc = $target.parents("h2").siblings(".descrip");
         if (desc.css("display") == "none") {
             desc.show();
         } else {
