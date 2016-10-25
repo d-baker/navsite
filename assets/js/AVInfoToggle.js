34738b5e421cc10d1db1a06ed28e3bf7cc0c57f1
dbaker <d-_-baker@hotmail.com>
2016-10-25 17:43:10 +1100

    making progress on audio embedding

--- assets/js/AVInfoToggle.js
+++ assets/js/AVInfoToggle.js
@@ -1,13 +1,35 @@
 $(document).ready(function() {
+    var sources = popSources();
+
 	$(".descrip").hide();
-    $("a.info").click(function(e) {
+    $(".info").click(function(e) {
         var $target = $(e.currentTarget);
         var desc = $target.parents("h2").siblings(".descrip");
+        var targetIndex = desc.closest(".avblock").find("li").index(desc.closest("li"));
+
         if (desc.css("display") == "none") {
+            console.log("Using target index " + targetIndex + " in display conditional");
+            console.log("Using source: " + sources.originalcomps[targetIndex]);
+
+            desc.find(".audioplayer").prepend(sources.originalcomps[targetIndex]);
             desc.show();
         } else {
+            desc.find(".audioplayer").remove("audio");
             desc.hide();
+            console.log("popping source and hiding");
         }
     });
+
+    function popSources() {
+        sources = {};
+
+        sources.originalcomps = $("#original-compositions audio");
+        sources.pianorecs = $("#piano-recordings audio");
+
+        $("audio").remove();
+
+        return sources;
+    }
+
 });
 
