e49daf4b76845666bbba918fc1f9c0f36353aee9
dbaker <d-_-baker@hotmail.com>
2016-10-25 18:01:32 +1100

    AV loading ACTUALLY working

--- assets/js/AVInfoToggle.js
+++ assets/js/AVInfoToggle.js
@@ -2,20 +2,36 @@ $(document).ready(function() {
     var sources = popSources();
 
 	$(".descrip").hide();
+
     $(".info").click(function(e) {
         var $target = $(e.currentTarget);
         var desc = $target.parents("h2").siblings(".descrip");
         var targetIndex = desc.closest(".avblock").find("li").index(desc.closest("li"));
 
         if (desc.css("display") == "none") {
-            console.log("Using target index " + targetIndex + " in display conditional");
-            console.log("Using source: " + sources.originalcomps[targetIndex]);
+            $(".descrip").find("audio").remove(); // remove all audios
+            $(".descrip").hide(); // hide all descrips
 
-            desc.find(".audioplayer").prepend(sources.originalcomps[targetIndex]);
+            // figure out which playlist the chosen track belongs to
+            var playlist;
+            if (desc.closest(".avblock").attr("id") == "original-compositions") {
+                playlist = sources.originalcomps;
+            } else if (desc.closest(".avblock").attr("id") == "piano-recordings") {
+                playlist = sources.pianorecs;
+            } else {
+                console.log("PANIC!");
+            }
+
+            console.log("Using target index " + targetIndex + " in display conditional");
+            console.log("Using source: " + playlist[targetIndex]);
+
+            // add audio to and show user's chosen descrip
+            desc.find(".audioplayer").prepend(playlist[targetIndex]);
             desc.show();
+
         } else {
-            desc.find("audio").remove();
-            desc.hide();
+            $(".descrip").find("audio").remove();
+            $(".descrip").hide();
             console.log("popping source and hiding");
         }
     });
