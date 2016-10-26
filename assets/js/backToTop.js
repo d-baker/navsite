950e4a7080e218db51928ea5081abb2f91029e16
dbaker <d-_-baker@hotmail.com>
2016-10-26 13:07:59 +1100

    fix back-to-top threshold

--- assets/js/backToTop.js
+++ assets/js/backToTop.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     $("#backtotop").hide();
 
     $(window).scroll(function() {
-        var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
-        if (scrollPercent > 5) {
+		var scrollPercent = ($(window).scrollTop() / $(document).height()) * 100;
+        if (scrollPercent > 10) {
             $("#backtotop").show();
         } else {
             $("#backtotop").hide();
