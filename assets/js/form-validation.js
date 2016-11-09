a767f1dd0b28ab0e2a732ae65e5aac73a9a91072
dbaker <d-_-baker@hotmail.com>
2016-11-09 19:11:10 +1100

    simplify and probably fix contact validation

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -1,47 +1,8 @@
 $(document).ready(function() {
 
-    var errorsFound = true;
-    var inputs = $("input, textarea").filter("[required]");
-
-    for (i = 0; i < inputs.length; i++) {
-        inp = $(inputs).get(i);
-        $(inp).change(function() {
-            validate(inp);
-        });
-        
-    }
-
-    function validate(input) {
-        if (invalid( $(input).val() ) || isARobot() ) {
-        } else {
-            errorsFound = false;
-            //var errorMessage = $(input).siblings(".error").get(0);
-            //$(errorMessage).show();
-        }
-    }
-
-    function invalid(input) {
-        if (input == undefined || input.trim() == "") {
-            console.log("input " + $(input).attr("name") + " is empty");
-            return true;
-        }
-        return false;
-    }
-
-    function isARobot() {
-        console.log("robot: " + $("#antirobot").val());
-
-        if ($("#antirobot").val() != "5") {
-            console.log("robot!!")
-            return false;
-        }
-        
-        console.log("not a robot")
-        return true;
-    }
-
     $("#submit").click(function(e) {
-        if (errorsFound == true) {
+        console.log($("#antirobot").val());
+        if ($("#antirobot").val() != "5") {
             e.preventDefault();
             $(".summary-error").show();
             $("#submit").effect("shake"); // nuh-uh!
