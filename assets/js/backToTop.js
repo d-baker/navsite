a5a9eca50acdd874cd7393bad6626f802dd4562d
dbaker <d-_-baker@hotmail.com>
2016-11-20 16:06:21 +1100

    begin migrating header.scss, delete unnecessary sass files, remove back to top button on desktop

--- assets/js/backToTop.js
+++ assets/js/backToTop.js
@@ -2,11 +2,17 @@ $(document).ready(function() {
     $("#backtotop").hide();
 
     $(window).scroll(function() {
-		var scrollPercent = ($(window).scrollTop() / $(document).height()) * 100;
-        if (scrollPercent > 10) {
-            $("#backtotop").show();
-        } else {
-            $("#backtotop").hide();
-        }
+
+    	if (! window.matchMedia("(min-width: 1300px)").matches) {
+
+			var scrollPercent = ($(window).scrollTop() / $(document).height()) * 100;
+	        if (scrollPercent > 10) {
+	            $("#backtotop").show();
+	        } else {
+	            $("#backtotop").hide();
+	        }
+	        
+	    }
+
     });
 });
