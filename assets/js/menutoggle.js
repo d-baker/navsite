5c36fe2e7997f06551f46e09f0012bccd2f9f5e2
dbaker <d-_-baker@hotmail.com>
2017-01-08 21:28:46 +1100

    vastly clean up menutoggle js

--- assets/js/menutoggle.js
+++ assets/js/menutoggle.js
@@ -1,42 +1,88 @@
 $(document).ready(function() {
 	$("header nav").addClass("closed");
-	$(".dropdown .nav-list").addClass("closed");
+	closeAllDropdowns();
 
     // Mobile menu toggle button handling
 	$("#menubutton").click(function() {
 		if ( $("header nav").hasClass("closed") ) {
 			$("header nav").removeClass("closed");
 			$("header nav").addClass("open");
+
 			$("#menubutton").attr("aria-expanded", "true")
 		} else if ( $("header nav").hasClass("open") ) {
 			$("header nav").removeClass("open");
 			$("header nav").addClass("closed");	
+
 			$("#menubutton").attr("aria-expanded", "false")	
 		}
 	});
 
+	// if on mobile...
 	if (! window.matchMedia("(min-width: 900px)").matches) {
-		// if on mobile, enable clicking on top level link to open dropdown menu
 		setMobileDropdownAttributes();
+
+		// enable clicking on top level link to open dropdown menu
 		openWithLandingLink();
 	}
+
 
     $(window).resize(function(){
         if (window.matchMedia("(min-width: 900px)").matches) {
     		// Prevent dropdown menus from staying open on desktop layout if opened in mobile layout
-			$(".dropdown .nav-list").removeClass("open");
-			$(".dropdown .nav-list").addClass("closed")
+			closeAllDropdowns();
+        	unsetMobileDropdownAttributes();
 
         	// Remove event handler that enforces preventDefault, to ensure we get default link functionality back on desktop
-        	unsetMobileDropdownAttributes();
         	$(".dropdown-landing").unbind("click");
         } else {
-        	// Bind default-preventing click handler for mobile
-        	// TODO when called here, preventDefault is working but other actions don't appear to be rebound??!
         	setMobileDropdownAttributes();
+
+        	// Bind default-preventing click handler for mobile
         	openWithLandingLink();
         }
     });
+
+
+	function openWithLandingLink() {
+		$(".dropdown-landing").click(function(e) {
+			e.preventDefault();
+			$target = $(e.currentTarget);
+
+			toggleAriaExpanded();
+
+			if ( $target.siblings(".nav-list").hasClass("closed") ) {
+				closeAllDropdowns();
+				openDropdown($target);
+			} else {
+				closeAllDropdowns();
+				closeDropdown($target);
+			}
+
+		});	
+	}
+
+	function toggleAriaExpanded(target) {
+		if ( $target.attr("aria-expanded") == "false" ) {
+			$target.attr("aria-expanded", "true");
+		} else {
+			$target.attr("aria-expanded", "false");			
+		}
+	}
+
+    function closeAllDropdowns() {
+		$(".dropdown .nav-list").removeClass("open");
+		$(".dropdown .nav-list").addClass("closed")
+    }
+
+    function closeDropdown(target) {
+		$target.siblings(".nav-list").removeClass("open");
+		$target.siblings(".nav-list").addClass("closed");
+    }
+
+    function openDropdown(target) {
+		$target.siblings(".nav-list").removeClass("closed");
+		$target.siblings(".nav-list").addClass("open");
+    }
 
     function setMobileDropdownAttributes() {
     	$(".dropdown-landing").attr("role", "button");
@@ -50,33 +96,5 @@ $(document).ready(function() {
     	$(".dropdown-landing").removeAttr("aria-label");
         $(".dropdown-landing").removeAttr("aria-expanded");
     }
-
-	function openWithLandingLink() {
-		$(".dropdown-landing").click(function(e) {
-			e.preventDefault();
-			$target = $(e.currentTarget);
-
-			if ( $target.attr("aria-expanded") == "false" ) {
-				$target.attr("aria-expanded", "true");
-			} else {
-				$target.attr("aria-expanded", "false");			
-			}
-
-			if ( $target.siblings(".nav-list").hasClass("closed") ) {
-				$(".dropdown .nav-list").removeClass("open");
-				$(".dropdown .nav-list").addClass("closed");
-
-				$target.siblings(".nav-list").removeClass("closed");
-				$target.siblings(".nav-list").addClass("open");
-			} else {
-				$(".dropdown .nav-list").removeClass("open");
-				$(".dropdown .nav-list").addClass("closed");
-
-				$target.siblings(".nav-list").removeClass("open");
-				$target.siblings(".nav-list").addClass("closed");
-			}
-
-		});	
-	}
 
 });
