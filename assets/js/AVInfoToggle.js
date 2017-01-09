4f36d5df128b0eaedd0e495da35afc7028afb630
dbaker <d-_-baker@hotmail.com>
2017-01-09 12:59:54 +1100

    solved the chrome preloading issue I think

--- assets/js/AVInfoToggle.js
+++ assets/js/AVInfoToggle.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-    var sources = popSources();
-
     setAlternateBGs();
 
 	$(".avblock .descrip").hide();
 
-    // stop li click propogating to children
+
+    // stop li clicks propogating to children
     $(".avblock li > .descrip").click(function(e) {
         e.stopPropagation();
     });
@@ -13,52 +12,24 @@ $(document).ready(function() {
         e.stopPropagation();
     });
 
+
     $(".avblock .playlist > li").click(function(e) {
         setAlternateBGs();
 
         var $target = $(e.currentTarget);
         var desc = $target.find(".descrip");
-        var targetIndex = $target.closest(".avblock").find(".playlist > li").index($target);
 
         if (desc.css("display") == "none") {
-            $(".avblock .descrip").find("audio, video").remove(); // remove all multimedia
             $(".avblock .descrip").hide(); // hide all descrips
 
-            // figure out which playlist the chosen track belongs to
-            var avBlockID = desc.closest(".avblock").attr("id");
-            var playlist;
-            if (avBlockID == "original-compositions") {
-                playlist = sources.originalcomps;
-            } else if (desc.closest(".avblock").attr("id") == "piano-recordings") {
-                playlist = sources.pianorecs;
-            } else {
-                console.log("Playlist " + avBlockID + " not found");
-            }
-
-            console.log("Using target index " + targetIndex + " in display conditional");
-            console.log("Using source: " + playlist[targetIndex]);
-
-            // add audio to and show user's chosen descrip
-            desc.find(".audioplayer, .videoplayer").prepend(playlist[targetIndex]);
+            // set user's chosen track bg to white and display
             desc.closest("li").css('background', "white");
             desc.show();
         } else {
-            $(".avblock .descrip").find("audio, video").remove();
             $(".avblock .descrip").hide();
-            console.log("popping source and hiding");
         }
     });
 
-    function popSources() {
-        sources = {};
-
-        sources.originalcomps = $("#original-compositions audio, #original-compositions video");
-        sources.pianorecs = $("#piano-recordings audio, #piano-recordings video");
-
-        $(".avblock .descrip audio, .avblock .descrip video").remove();
-
-        return sources;
-    }
 
     function setAlternateBGs() {
         $.each($(".avblock"), function(i, el) {
