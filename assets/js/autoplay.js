fd5351a051bbd110e221b7e9b3813c92d9fd4a16
d-baker <d-_-baker@hotmail.com>
2017-10-16 12:29:16 +1100

    factor out autoplay script into separate file

---
+++ assets/js/autoplay.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $("audio").on("ended", function(e) {
+        var nextItem = $(e.target).closest("li").next("li");
+        var nextSong = $(nextItem).find("audio");
+
+        if ($(nextSong)) {
+            setAlternateBGs();
+            $(".playlist button").attr("aria-expanded", "false");
+            $(".avblock .descrip").hide(); // hide all descrips
+
+            // set user's chosen track bg to white and display
+            var desc = $(nextItem).find(".descrip");
+            desc.closest("li").css('background', "white");
+            desc.siblings("button").first().attr("aria-expanded", "true");
+            desc.show();
+
+            $(nextSong).trigger("play");
+        }
+    });
+
+    function setAlternateBGs() {
+        $.each($(".avblock"), function(i, el) {
+            var LIs = $(this).find(".playlist > li");
+
+            $.each(LIs, function (j, li) {
+                if (j % 2 != 0) {
+                    $(this).css("background", "#ddd");
+                } else {
+                    $(this).css("background", "none")
+                }
+            });
+        });
+    }
+});
