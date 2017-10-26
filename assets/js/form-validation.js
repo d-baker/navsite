92f404e2ee85d85b0c89ae5bd6711dcfa581ed14
d-baker <d-_-baker@hotmail.com>
2017-10-26 21:03:49 +1100

    Reinstate antirobot field

---
+++ assets/js/form-validation.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+        $("#submit").click(function(e) {
+                    console.log($("#antirobot").val());
+                            if ($("#antirobot").val() != "5") {
+                                            e.preventDefault();
+                                                        $(".summary-error").show();
+                                                                    $("#submit").effect("shake"); // nuh-uh!
+                                                                            }
+                                });
+
+});
